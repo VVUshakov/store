401c00e140f9f4f1a06296415fccadf3be973743
VVUshakov <svayznoy@inbox.ru>
2022-09-24 22:48:59 +0700

    Добавить книгу в корзину

---
+++ presentation/Store.Web/SessionExtensions.cs
@@ -0,0 +1,60 @@
+﻿using Store.Web.Models;
+using System.Text;
+
+namespace Store.Web
+{
+    public static class SessionExtensions
+    {
+        public const string key = "Cart";
+
+        public static void Set(this ISession session, Cart value)
+        {
+            if (value == null)
+                return;
+
+            using (var stream = new MemoryStream())
+                using (var writer = new BinaryWriter(stream, Encoding.UTF8, true))
+                {
+                    writer.Write(value.Items.Count);
+
+                    foreach (KeyValuePair<int,int> item in value.Items)
+                    {
+                        writer.Write(item.Key);
+                        writer.Write(item.Value);
+                    }
+
+                    writer.Write(value.Amount);
+
+                    session.Set(key, stream.ToArray());
+                }
+        }
+        
+        public static bool TryGetCart(this ISession session, out Cart value)
+        {
+            if(session.TryGetValue(key, out byte[] buffer))
+            {
+                using (var stream = new MemoryStream(buffer))
+                    using (var reader = new BinaryReader(stream, Encoding.UTF8, true))
+                    {
+                        value = new Cart();
+
+                        int length = reader.ReadInt32();
+                        for(int i = 0; i < length; i++)
+                        {
+                            int bookId = reader.ReadInt32();
+                            int count = reader.ReadInt32();
+
+                            value.Items.Add(bookId, count);
+                        }
+                         
+                        value.Amount = reader.ReadDecimal();
+
+                        return true;
+                    }
+            }
+
+            value = null;
+            return false;
+        }
+    }
+}
--- presentation/Store.Web/Startup.cs
+++ presentation/Store.Web/Startup.cs
@@ -12,13 +12,24 @@ namespace Store.Web
 
         public IConfiguration Configuration {get;}
 
+        // Этот метод вызывается средой выполнения. Используйте этот метод для добавления служб в контейнер.
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDistributedMemoryCache();
+            services.AddSession(option =>
+            {
+                option.IdleTimeout = TimeSpan.FromMinutes(20); //время жизни сессии
+                option.Cookie.HttpOnly = true;
+                option.Cookie.IsEssential = true; //технические куки для работы сервиса не требующие разрешения от пользователя
+            });
             services.AddSingleton<IBookRepository, BookRepository>();
             services.AddSingleton<BookService>();
         }
 
+        // Этот метод вызывается средой выполнения. Используйте этот метод для настройки конвейера HTTP-запросов.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -28,13 +39,19 @@ namespace Store.Web
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                // Значение HSTS по умолчанию — 30 дней. Вы можете изменить это для рабочих сценариев, см. https://aka.ms/aspnetcore-hsts.
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+
             app.UseRouting();
+            
             app.UseAuthorization();
+
+            app.UseSession();
 
             app.UseEndpoints(endpoints =>
             {
